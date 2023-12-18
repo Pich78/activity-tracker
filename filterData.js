2634ed4462966711cfed079999f453113e832eb0
apicciano <antonio.picciano@datalogic.com>
2023-12-18 16:10:54 +0100

    Removed logs

--- filterData.js
+++ filterData.js
@@ -1,12 +1,9 @@
 function filterData(activity, days, data_array) {
-    console.log("Filter Data")
+
     let cutoff = new Date();
-    console.log("Today's date: " + cutoff)
     cutoff.setDate(cutoff.getDate() - days);
-    console.log("Past date: " + cutoff)
 
     return data_array.filter(item => {
-        console.log("Data from item: " + item.date)
 
         let parts = item.date.split('_');
         let year = parseInt(parts[0], 10);
@@ -14,7 +11,6 @@ function filterData(activity, days, data_array) {
         let day = parseInt(parts[2], 10);
         let itemDate = new Date(year, month, day);
 
-        console.log("Converted item date: " + itemDate)
         if (item.activity === activity) {
             console.log(itemDate)
             console.log(cutoff)
