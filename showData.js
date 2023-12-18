71257d0f1ad225413533a51f791891d03d75e325
apicciano <antonio.picciano@datalogic.com>
2023-12-18 14:43:22 +0100

    d3.js called in a function

---
+++ showData.js
@@ -0,0 +1,43 @@
+function showData(data) {
+    // Remove the old svg if it exists
+    d3.select("#chart").select("svg").remove();
+
+    // Set the dimensions and margins of the graph
+    let margin = {top: 20, right: 20, bottom: 70, left: 40},
+        width = 600 - margin.left - margin.right,
+        height = 300 - margin.top - margin.bottom;
+
+    // Set the ranges
+    let x = d3.scaleBand().range([0, width]).padding(0.1);
+    let y = d3.scaleLinear().range([height, 0]);
+
+    // Append the svg object to the div
+    let svg = d3.select("#chart").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    // Scale the range of the data in the domains
+    x.domain(data.map(function(d) { return d.date; }));
+    y.domain([0, d3.max(data, function(d) { return d.value; })]);
+
+    // Append the rectangles for the bar chart
+    svg.selectAll(".bar")
+        .data(data)
+        .enter().append("rect")
+        .attr("class", "bar")
+        .attr("x", function(d) { return x(d.date); })
+        .attr("width", x.bandwidth())
+        .attr("y", function(d) { return y(d.value); })
+        .attr("height", function(d) { return height - y(d.value); });
+
+    // Add the x Axis
+    svg.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x));
+
+    // Add the y Axis
+    svg.append("g")
+        .call(d3.axisLeft(y));
+}
