e0652f295995256cc0298720d95b0a27d1013691
apicciano <antonio.picciano@datalogic.com>
2023-12-18 16:50:49 +0100

    Changed style

--- showData.js
+++ showData.js
@@ -33,15 +33,23 @@ function showData(data) {
         .attr("height", function(d) { return height - y(d.value); })
         .style("fill", "#3F51B5") // Use a Material Design color
         .style("filter", "url(#drop-shadow)") // Add a drop shadow
-        .transition() // Add a transition
-        .duration(1000)
-        .attr("y", function(d) { return y(d.value); })
-        .attr("height", function(d) { return height - y(d.value); });
+        .on("mouseover", function(d) { // Add interactivity
+            d3.select(this).style("fill", "#303F9F"); // Darken the bar
+            tooltip.style("visibility", "visible"); // Show the tooltip
+            tooltip.text(formatDate(d.date) + ", " + d.value + " units"); // Set the tooltip text
+        })
+        .on("mousemove", function() { // Move the tooltip with the mouse
+            tooltip.style("top", (d3.event.pageY-10)+"px").style("left",(d3.event.pageX+10)+"px");
+        })
+        .on("mouseout", function() { // Reset the bar and hide the tooltip
+            d3.select(this).style("fill", "#3F51B5");
+            tooltip.style("visibility", "hidden");
+        });
 
     // Add the x Axis
     svg.append("g")
         .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(x));
+        .call(d3.axisBottom(x).tickFormat(d3.timeFormat("%d/%m"))); // Format the dates as DD/MM
 
     // Add the y Axis
     svg.append("g")
@@ -66,4 +74,19 @@ function showData(data) {
         .attr("in", "offsetBlur");
     feMerge.append("feMergeNode")
         .attr("in", "SourceGraphic");
+
+    // Define the tooltip
+    let tooltip = d3.select("body")
+        .append("div")
+        .style("position", "absolute")
+        .style("z-index", "10")
+        .style("visibility", "hidden")
+        .style("background", "#FFFFFF")
+        .style("border", "1px solid #DDDDDD")
+        .style("border-radius", "5px")
+        .style("padding", "5px")
+        .style("box-shadow", "2px 2px 5px #BBBBBB");
+
+    // Define the date format
+    let formatDate = d3.timeFormat("%A %d %B");
 }
