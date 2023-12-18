560270ba2b86f1f522544e079b0d9da61026b363
apicciano <antonio.picciano@datalogic.com>
2023-12-18 18:19:35 +0100

    Changed data format in x parameter

--- showData.js
+++ showData.js
@@ -27,7 +27,7 @@ function showData(data) {
         .data(data)
         .enter().append("rect")
         .attr("class", "bar")
-        .attr("x", function(d) { return x(d.date); })
+        .attr("x", function(d) { return x(dateTransform(d.date)); })
         .attr("width", x.bandwidth())
         .attr("y", function(d) { return y(d.value); })
         .attr("height", function(d) { return height - y(d.value); })
