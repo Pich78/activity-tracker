e066cb641a3dc6673d792213b96e43c8f62fd940
apicciano <antonio.picciano@datalogic.com>
2023-12-18 13:31:59 +0100

    fetchData transformed with async/await

--- fetchData.js
+++ fetchData.js
@@ -1,25 +1,24 @@
 // fetchData.js
-function fetchData(path) {
-    return fetch(path)
-        .then(response => response.json())
-        .then(files => {
-            let promises = files.map(file => {
-                return fetch(file.download_url)
-                    .then(response => response.json())
-                    .then(fileData => {
-                        let date = file.name.substring(0, 10); // extract date from file name
-                        return fileData.map(activity => {
-                            return {
-                                date: date,
-                                activity: activity.activity,
-                                value: activity.value,
-                                unit: activity.unit
-                            };
-                        });
-                    });
+async function fetchData(path) {
+    try {
+        const response = await fetch(path);
+        const files = await response.json();
+        let promises = files.map(async file => {
+            const fileResponse = await fetch(file.download_url);
+            const fileData = await fileResponse.json();
+            let date = file.name.substring(0, 10); // extract date from file name
+            return fileData.map(activity => {
+                return {
+                    date: date,
+                    activity: activity.activity,
+                    value: activity.value,
+                    unit: activity.unit
+                };
             });
-            return Promise.all(promises);
-        })
-        .then(dataArrays => [].concat(...dataArrays)) // flatten the array of arrays
-        .catch(error => console.error(error));
+        });
+        let dataArrays = await Promise.all(promises);
+        return [].concat(...dataArrays); // flatten the array of arrays
+    } catch (error) {
+        console.error(error);
+    }
 }
