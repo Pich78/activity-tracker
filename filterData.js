b07aca506db644fb3a67d879db53e23332f7ac3a
apicciano <antonio.picciano@datalogic.com>
2023-12-18 15:51:53 +0100

    Added debug

--- filterData.js
+++ filterData.js
@@ -1,11 +1,14 @@
 function filterData(activity, days, data_array) {
     console.log("Filter Data")
     let cutoff = new Date();
-    console.log(cutoff)
+    console.log("Today's date: " + cutoff)
     cutoff.setDate(cutoff.getDate() - days);
+    console.log("Past date: " + cutoff)
 
     return data_array.filter(item => {
+        console.log("Data from item: " + item.date)
         let itemDate = new Date(item.date);
+        console.log("Converted item date: " + item.date)
         if (item.activity === activity) {
             console.log(itemDate)
             console.log(cutoff)
